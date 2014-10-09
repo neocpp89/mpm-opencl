3f22c06c4e55e1802c6d110a0f5ff9797ec9f8b6
Sachith Dunatunga <neocpp@gmail.com>
2014-10-09 02:40:16 -0400

    Wrote tests for small tensor class.

--- sim/Tensor.hpp
+++ sim/Tensor.hpp
@@ -1,17 +1,140 @@
-#ifndef MATRIX_HPP 
-#define MATRIX_HPP
+#ifndef TENSOR_HPP
+#define TENSOR_HPP
 #include <array>
+#include <exception>
+#include <cmath>
+#include <utility>
+
+#include <iostream>
+
 template <typename Real, size_t dimension>
 class Tensor
 {
     private:
         std::array<Real, dimension*dimension> data;
-    
-    public:
 
-        Real & operator()(size_t i, size_t j) { return data[i * dimension + j]; }
-        Real operator()(size_t i, size_t j) const { return data[i * dimension + j]; }
-        size_t dim() { return dimension; }
+        size_t linearIndex(const size_t i, const size_t j) const { return (i * dimension + j); }
+
+    public:
+        Tensor() { return; }
+        Tensor(const std::array<Real, dimension*dimension> A)
+        {
+            for (size_t i = 0; i < dimension; i++) {
+                for (size_t j = 0; j < dimension; j++) {
+                    data[linearIndex(i, j)] = A[linearIndex(i, j)];
+                }
+            }
+
+            return;
+        }
+        Tensor(const Tensor &A)
+        {
+            for (size_t i = 0; i < A.dim(); i++) {
+                for (size_t j = 0; j < A.dim(); j++) {
+                    data[linearIndex(i, j)] = A(i, j);
+                }
+            }
+            return;
+        }
+        ~Tensor() { return; }
+
+        size_t dim() const { return dimension; }
+
+        Real trace()
+        {
+            Real tr = 0;
+            for (size_t i = 0; i < dimension; i++) {
+                tr += data[linearIndex(i, i)];
+            }
+            return tr;
+        }
+
+        void decompose(Tensor &deviator, Real &spherical)
+        {
+            spherical = this->trace() / (Real)dimension;
+            for (size_t i = 0; i < dimension; i++) {
+                for (size_t j = 0; j < dimension; j++) {
+                    deviator(i, j) = data[linearIndex(i, j)];
+                }
+                deviator(i, i) -= spherical;
+            }
+        }
+
+        Tensor deviator()
+        {
+            Tensor dev;
+            Real tr;
+            this->decompose(dev, tr);
+            return dev;
+        }
+
+        Real contraction(const Tensor &A)
+        {
+            Real s = 0;
+
+            for (size_t i = 0; i < dimension; i++) {
+                for (size_t j = 0; j < dimension; j++) {
+                    s += A(i, j) * data[linearIndex(i, j)];
+                }
+            }
+
+            return s;
+        }
+
+        Real frobeniusNorm()
+        {
+            return std::sqrt(contraction(*this));
+        }
+
+        Real & operator()(const size_t i, const size_t j) { return data[linearIndex(i, j)]; }
+        Real operator()(const size_t i, const size_t j) const { return data[linearIndex(i, j)]; }
+
+        void swap(Tensor &A)
+        {
+            std::swap(this->data, A.data);
+            return;
+        }
+
+        Tensor & operator=(Tensor A)
+        {
+            A.swap(*this);
+            return (*this);
+        }
+
+        inline bool operator==(const Tensor &rhs) const
+        {
+            if (dimension != rhs.dim()) {
+                return false;
+            }
+
+            for (size_t i = 0; i < dimension; i++) {
+                for (size_t j = 0; j < dimension; j++) {
+                    if (data[linearIndex(i, j)] != rhs(i, j)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        inline bool operator!=(const Tensor &rhs) const
+        {
+            return !operator==(rhs);
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Tensor& A)
+        {
+            os << "\n[\n";
+            for (size_t i = 0; i < A.dim(); i++) {
+                os << "  ";
+                for (size_t j = 0; j < A.dim(); j++) {
+                    os << A(i,j) << " ";
+                }
+                os << "\n";
+            }
+            os << "]";
+            return os;
+        }
 };
 
-#endif //MATRIX_HPP
+#endif //TENSOR_HPP
--- tests/TensorTest.cpp
+++ tests/TensorTest.cpp
@@ -3,29 +3,87 @@
 #include "TestUtils.hpp"
 #include "Tensor.hpp"
 
+double myfunc(size_t seed)
+{
+    Tensor<double, 3> T;
+    Tensor<double, 3> T0;
+    double pr;
+    for (int i = 0; i < T.dim(); i++) {
+        for (int j = 0; j < T.dim(); j++) {
+            T(i,j) = (seed * i + j) ^ seed;
+        }
+    }
+    T.decompose(T0, pr);
+    return T0.frobeniusNorm();
+}
+
 int main(int argc, char **argv)
 {
-    Tensor<int, 3> S;
+    Tensor<double, 3> S({1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0});
 
+    //Output tensor
+    std::cout << "S = " << S << std::endl;
+
+    // Test dimensions
     TEST(S.dim() == 3);
-    
-    for (int s = 0; s < 3; s++) {
-        for (int x = 0; x < 3; x++) {
-            S(s,x) = s+x;
+
+    // Test initialization
+    bool initializationTest = true;
+    for (size_t i = 0; i < S.dim(); i++) {
+        for (size_t j = 0; j < S.dim(); j++) {
+            if(S(i, j) != 1.0 + i * S.dim() + j) {
+                initializationTest = false;
+            }
         }
     }
+    TEST(initializationTest);
 
-    TEST(S(0,0) == 2);
-
-
-    for (int s = 0; s < 3; s++) {
-        for (int x = 0; x < 3; x++) {
-            std::cout << S(s,x) << std::endl;
+    // Test copy constructor
+    Tensor<double, 3> A(S);
+    bool copyConstructorTest = true;
+    for (size_t i = 0; i < A.dim(); i++) {
+        for (size_t j = 0; j < A.dim(); j++) {
+            if(S(i, j) != A(i, j)) {
+                copyConstructorTest = false;
+            }
         }
     }
+    std::cout << "A = " << A << std::endl;
+    TEST(copyConstructorTest);
     
-    std::cout << TerminalColors::PassedString << std::endl;
-    std::cout << "trololol" << std::endl;
+    // Test equality operator
+    TEST(S == A);
+    Tensor<double, 3> B(A);
+    B(0,0) = 2;
+    std::cout << "B = " << B << std::endl;
+    TEST(S != B);
 
+    // Test trace
+    TEST(S.trace() == 15.0);
+    TEST(B.trace() == 16.0);
+
+    // Test deviator
+    Tensor<double, 3> expectedA0({-4,2,3,4,0,6,7,8,4});
+    Tensor<double, 3> A0;
+    double sphA;
+    A.decompose(A0, sphA);
+    std::cout << "A0 = " << A0 << std::endl;
+    TEST(A.trace() == A.dim() * sphA);
+    TEST(A0 == expectedA0);
+
+    // Test contraction
+    TEST(A0.contraction(A) == 210.0);
+    TEST(A0.contraction(B) == 206.0);
+
+    /* 
+    double c = 0;
+    for (size_t seed = 1; seed < 10000000L; seed++) {
+        c += myfunc(seed);
+        // std::cout << myfunc(seed) << std::endl;
+    }
+    std::cout << c << std::endl;
+    */
+
+    std::cout << TerminalColors::Colorize("All tests passed.", TerminalColors::FG_GREEN) << std::endl;
     return 0;
 }
