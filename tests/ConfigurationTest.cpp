222174a5da7c93848d12a2206474f7a34e6ffa7b
Sachith Dunatunga <neocpp@gmail.com>
2015-04-28 11:17:21 -0400

    Modified configuration test to use catch.
    Probably need to redo file loading.

--- tests/ConfigurationTest.cpp
+++ tests/ConfigurationTest.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "TestUtils.hpp"
+#include "catch.hpp"
 #include "Configuration.hpp"
 
-int main(int argc, char **argv)
+TEST_CASE("Static Configuration Initialization", "[configuration][short]")
 {
     StaticConfiguration scfg;
     Configuration *cfg = dynamic_cast<Configuration *>(&scfg);
@@ -13,30 +13,54 @@ int main(int argc, char **argv)
     scfg.addIntOption("int_option", 36);
     scfg.addStringOption("string_option", "Hello, World!");
     scfg.addSizeTOption("sizet_option", 203L);
-    TEST(cfg->getDoubleOption("double_option") == 0.25);
-    TEST(cfg->getIntOption("int_option") == 36);
-    TEST(cfg->getStringOption("string_option") == "Hello, World!");
-    TEST(cfg->getSizeTOption("sizet_option") == 203L);
+    REQUIRE(cfg->getDoubleOption("double_option") == 0.25);
+    REQUIRE(cfg->getIntOption("int_option") == 36);
+    REQUIRE(cfg->getStringOption("string_option") == "Hello, World!");
+    REQUIRE(cfg->getSizeTOption("sizet_option") == 203L);
+}
+
+TEST_CASE("Static Configuration Modification", "[configuration][short]")
+{
+    StaticConfiguration scfg;
+    Configuration *cfg = dynamic_cast<Configuration *>(&scfg);
+
+    // set up one of each type of option
+    scfg.addDoubleOption("double_option", 0.25);
+    scfg.addIntOption("int_option", 36);
+    scfg.addStringOption("string_option", "Hello, World!");
+    scfg.addSizeTOption("sizet_option", 203L);
 
     // try to change each of them
     scfg.setDoubleOption("double_option", 0.5);
     scfg.setIntOption("int_option", -8);
     scfg.setStringOption("string_option", "bar");
     scfg.setSizeTOption("sizet_option", 9L);
-    TEST(cfg->getDoubleOption("double_option") == 0.5);
-    TEST(cfg->getIntOption("int_option") == -8);
-    TEST(cfg->getStringOption("string_option") == "bar");
-    TEST(cfg->getSizeTOption("sizet_option") == 9L);
+    REQUIRE(cfg->getDoubleOption("double_option") == 0.5);
+    REQUIRE(cfg->getIntOption("int_option") == -8);
+    REQUIRE(cfg->getStringOption("string_option") == "bar");
+    REQUIRE(cfg->getSizeTOption("sizet_option") == 9L);
+}
+
+TEST_CASE("Static Configuration File Read", "[configuration][short]")
+{
+    StaticConfiguration scfg;
+    Configuration *cfg = dynamic_cast<Configuration *>(&scfg);
+
+    // set up one of each type of option
+    scfg.addDoubleOption("double_option", 0.25);
+    scfg.addIntOption("int_option", 36);
+    scfg.addStringOption("string_option", "Hello, World!");
+    scfg.addSizeTOption("sizet_option", 203L);
 
     // actually parse a configuration file specified to override defaults
     const std::string cfgfile = "test.cfg";
     //std::cout << "Read configuration file (\"" << cfgfile << "\")." << std::endl;
     scfg.readConfigFile(cfgfile);
     // std::cout << "Configuration: " << scfg << std::endl;
-    TEST(cfg->getDoubleOption("double_option") == 45.25);
-    TEST(cfg->getIntOption("int_option") == 32);
-    TEST(cfg->getStringOption("string_option") == "foo bar");
-    TEST(cfg->getSizeTOption("sizet_option") == 352L);
+    REQUIRE(cfg->getDoubleOption("double_option") == 45.25);
+    REQUIRE(cfg->getIntOption("int_option") == 32);
+    REQUIRE(cfg->getStringOption("string_option") == "foo bar");
+    REQUIRE(cfg->getSizeTOption("sizet_option") == 352L);
 
     // try to read a nonexistent key
     bool nonexistentKeyTest = false;
@@ -48,9 +72,6 @@ int main(int argc, char **argv)
     } catch (std::out_of_range &e) {
         nonexistentKeyTest = true;
     }
-    TEST(nonexistentKeyTest);
-
-    ALLPASSED(argv[0]);
-    return 0;
+    REQUIRE(nonexistentKeyTest == true);
 }
 
