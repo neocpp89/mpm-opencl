7bab1de2d9dba8040563e72dbbddc2e74f1c23b3
Sachith Dunatunga <neocpp@gmail.com>
2015-05-30 01:00:24 -0400

    Added gradient of shape functions to master.
    Test needs to be made more thorough.

--- sim/MasterElement.hpp
+++ sim/MasterElement.hpp
@@ -9,6 +9,10 @@ template <typename Real>
 struct Point2
 {
     Real x, y;
+    bool operator==(const Point2<Real> &rhs)
+    {
+        return ((x == rhs.x) && (y == rhs.y));
+    }
 };
 
 template <typename Real>
@@ -54,6 +58,38 @@ class P1Quad
             return 0.25 * (1.0 - p.x) * (1.0 + p.y);
         }
 
+        Point2<Real> gradphi0(const Point2<Real> &p) const
+        {
+            Point2<Real> grad;
+            grad.x = -0.25 * (1.0 - p.y);
+            grad.y = -0.25 * (1.0 - p.x);
+            return grad;
+        }
+
+        Point2<Real> gradphi1(const Point2<Real> &p) const
+        {
+            Point2<Real> grad;
+            grad.x = 0.25 * (1.0 - p.y);
+            grad.y = -0.25 * (1.0 + p.x);
+            return grad;
+        }
+
+        Point2<Real> gradphi2(const Point2<Real> &p) const
+        {
+            Point2<Real> grad;
+            grad.x = 0.25 * (1.0 + p.y);
+            grad.y = 0.25 * (1.0 + p.x);
+            return grad;
+        }
+
+        Point2<Real> gradphi3(const Point2<Real> &p) const
+        {
+            Point2<Real> grad;
+            grad.x = -0.25 * (1.0 + p.y);
+            grad.y = 0.25 * (1.0 - p.x);
+            return grad;
+        }
+
     public:
         template <typename Iterator>
         void phi(const Point2<Real> &p, Iterator begin, Iterator end) const
@@ -72,6 +108,28 @@ class P1Quad
             *begin = phi2(p);
             begin++;
             *begin = phi3(p);
+            begin++;
+
+            return;
+        }
+
+        template <typename Iterator>
+        void gradphi(const Point2<Real> &p, Iterator begin, Iterator end) const
+        {
+            if (check_writes) {
+                std::size_t dist = std::distance(begin, end);
+                if (dist < getNumberOfNodes()) {
+                    throw std::runtime_error("Output not large enough.");
+                }
+            }
+
+            *begin = gradphi0(p);
+            begin++;
+            *begin = gradphi1(p);
+            begin++;
+            *begin = gradphi2(p);
+            begin++;
+            *begin = gradphi3(p);
             begin++;
 
             return;
--- tests/MasterElementTest.cpp
+++ tests/MasterElementTest.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "MasterElement.hpp"
 
-TEST_CASE("P1 Quad Shapefunctions", "[master-element][short]")
+TEST_CASE("P1 Quad Shape Functions", "[master-element][short]")
 {
     master_element::Point2<double> n0 = {-1, -1};
     master_element::Point2<double> n1 = {1, -1};
@@ -74,3 +74,29 @@ TEST_CASE("P1 Quad Shapefunctions", "[master-element][short]")
     REQUIRE(result[3] == 0);
 }
 
+TEST_CASE("P1 Quad Gradient of Shape Functions", "[master-element][short]")
+{
+    master_element::Point2<double> n0 = {-1, -1};
+    master_element::Point2<double> n1 = {1, -1};
+    master_element::Point2<double> n2 = {1, 1};
+    master_element::Point2<double> n3 = {-1, 1};
+    master_element::Point2<double> mid = {0, 0};
+    master_element::Point2<double> midhr = {1, 0};
+    master_element::Point2<double> midhl = {-1, 0};
+    master_element::Point2<double> midvt = {0, 1};
+    master_element::Point2<double> midvb = {0, -1};
+
+    master_element::P1Quad<double> master;
+
+    std::vector<master_element::Point2<double>> result(4, {0, 0});
+    master_element::Point2<double> expected_g0 = {-0.5, -0.5};
+    master_element::Point2<double> expected_g1 = {0.5, 0};
+    master_element::Point2<double> expected_g2 = {0, 0};
+    master_element::Point2<double> expected_g3 = {0, 0.5};
+
+    master.gradphi(n0, result.begin(), result.end());
+    REQUIRE(result[0] == expected_g0);
+    REQUIRE(result[1] == expected_g1);
+    REQUIRE(result[2] == expected_g2);
+    REQUIRE(result[3] == expected_g3);
+}
